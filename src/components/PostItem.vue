8d0960b8cc0df28cec9d439c5dbe3b1ef9504810
andrey <andrey.burlyaev@gmail.com>
2024-06-20 17:32:47 +0300

    correction 1.0

--- src/components/PostItem.vue
+++ src/components/PostItem.vue
@@ -47,34 +47,3 @@
         border-radius: 12px;
 }
 </style>
-
- <!-- <template>
-    <div class="post">
-        <div>
-            <div>{{ post.id }} </div>
-            <div><strong>Название:</strong> {{ post.title }}</div>
-            <div><strong>Описание:</strong> {{ post.body }}</div>
-        </div>
-        <div class="post__btns">
-            <my-button class='hover-button'  @click="openPost(post)">Открыть</my-button>
-            <my-button class='hover-button'  @click="$emit('remove', post.id)">Удалить</my-button>
-        </div>
-    </div>
-</template>  -->
-
- <!-- <style>
-    .post {
-        background-color: white; 
-        color: black; 
-        padding: 15px;
-        border: 2px solid rgb(93, 210, 210);
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-        border-radius: 12px;
-    }
-    .post__btns{
-        display: flex;
-        gap: 10px
-    }
-</> -->
